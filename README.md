5918edbb9fcdc1d34d2251d92aaadcbce974768d
Nikolajus Karpovas <superheromatch@gmail.com>
2019-09-07 21:19:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Kafka Admin
+This repo contains all documentation on Kafka cluster for Superher Match.
+
+### Superheroville Municipality
+In this package all the relevant Kafka information (`topics`, `data`, `commands`) related to the `superheroville-municipality` project.
---
+++ superheroville-municiality/commands.js
@@ -0,0 +1,5 @@
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic store.municipality.superhero --create --partitions 3 --replication-factor 1
+
+// kafka-topics.sh --bootstrap-server localhost:9092 --list
+
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic store.municipality.superhero --describe
