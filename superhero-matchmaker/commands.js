1104b562fbe57b2eefa6cbd294e52c9b12ec75fc
Nikolajus Karpovas <superheromatch@gmail.com>
2019-09-08 16:51:37 +0200

    Add superhero-matchmaker info

---
+++ superhero-matchmaker/commands.js
@@ -0,0 +1,5 @@
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic store.matchmaker.choice --create --partitions 3 --replication-factor 1
+
+// kafka-topics.sh --bootstrap-server localhost:9092 --list
+
+// kafka-topics.sh --bootstrap-server localhost:9092 --topic store.matchmaker.choice --describe
