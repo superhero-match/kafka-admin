c7eaf4ce98fa59f220d7e5be26fbfca73a85be9b
Nikolajus Karpovas <superheromatch@gmail.com>
2020-10-02 14:23:24 +0200

    Add all create topic commands

---
+++ create_topics.js
@@ -0,0 +1,25 @@
+kafka-topics.bat --bootstrap-server localhost:9092 --topic store.municipality.superhero --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic delete.municipality.profilepicture --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic delete.chat.message --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic update.firebase.token --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic delete.match.match --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic store.chat.message --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic store.choice.choice --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic delete.municipality.superhero --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic store.match.match --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic store.municipality.superhero --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic update.municipality.superhero --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic update.municipality.profilepicture --create --partitions 3 --replication-factor 1
+
+kafka-topics.bat --bootstrap-server localhost:9092 --topic store.report.report --create --partitions 3 --replication-factor 1
--- superheroville-municiality/commands.js
+++
@@ -1,5 +0,0 @@
-// kafka-topics.sh --bootstrap-server localhost:9092 --topic store.municipality.superhero --create --partitions 3 --replication-factor 1
-
-// kafka-topics.sh --bootstrap-server localhost:9092 --list
-
-// kafka-topics.sh --bootstrap-server localhost:9092 --topic store.municipality.superhero --describe
